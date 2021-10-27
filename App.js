d2aee9139a85a8d3e3978ebf546ca903f0e91cf2
AlexHM <alejandro.herrera@utags.edu.mx>
2021-10-27 16:12:29 -0500

    Se creo la base y se agrego los primeros componentes

--- App.js
+++ App.js
@@ -1,13 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Counter from './components/Pages/Counter';
+import AddProduct from './components/Pages/AddProduct';
 
-export default function App() {
+function ProductScreen({ product }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>{product}!</Text>
     </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+export default function App() {
+
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    console.log(products);
+  }, [products])
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Counter") {
+              iconName = focused
+                ? "hourglass"
+                : "hourglass-outline";
+            } else if (route.name === "Add product") {
+              iconName = focused
+                ? "add-circle"
+                : "add-circle-outline";
+            } else if (route.name === "Cart") {
+              iconName = focused
+                ? "cart"
+                : "cart-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "grey",
+        })}
+      >
+        <Tab.Screen name="Counter" component={Counter} />
+        <Tab.Screen name="Add product" component={AddProduct} initialParams={{ handleProducts: setProducts }} />
+        <Tab.Screen name="Cart" component={ProductScreen} initialParams={products} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
